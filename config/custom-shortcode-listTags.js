9cf074eac42dbb3e0b9e2b1b3d3a690d26dd7e15
Nriquedev <nriquedev@gmail.com>
2024-03-19 17:15:19 +0100

    Created shortcode to list post tags

---
+++ config/custom-shortcode-listTags.js
@@ -0,0 +1,6 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addShortcode("listTagsExcept", async function(tags, excludedTags) {
+        let filteredTags = tags.filter(tag => !excludedTags.includes(tag));
+        return filteredTags.join(", ");
+    });
+};
